138edcc14635259ad30ca65d615a9732816de78a
Vaglan <45508237+therobots@users.noreply.github.com>
2026-06-12 12:12:47 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import WhatsAppWidget from './WhatsAppWidget';
 
 export default function Layout() {
+  const location = useLocation();
+
+  // Scroll to top on every route or search parameter change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname, location.search]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
